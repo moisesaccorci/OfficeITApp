e3e651f2dccb6fc6730d7e72f1e83c3d311b7ce7
Moises Accorci <moisesaccorci@MacBook-Pro-de-Moises.local>
2021-07-01 00:31:43 -0300

    Modified Home

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -5,7 +5,7 @@ import { Button } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import Home from '../screens/home/Home';
 import Classroom from '../screens/home/Classroom';
-import {Action} from '../screens/home/Classroom';
+import { Action } from '../screens/home/Classroom';
 import Contact from '../screens/home/Contact';
 import Store from '../screens/store/Store';
 import Courses from '../screens/store/Courses';
@@ -16,33 +16,22 @@ const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 const MyTheme = {
-   dark: false,
-   colors: {
-    primary: 'white',
-    background: 'rgb(242, 242, 242)',
-    card: '#01458e',
-    text: 'white',
-    border: '#0769d3',
-    notification: 'rgb(255, 69, 58)',
-   }
+    dark: false,
+    colors: {
+        primary: 'white',
+        background: 'rgb(242, 242, 242)',
+        card: '#01458e',
+        text: 'white',
+        border: '#0769d3',
+        notification: 'rgb(255, 69, 58)',
+    }
 }
 
 export default function Navigators() {
 
     return (
-        <NavigationContainer theme={MyTheme}> 
-            <Tab.Navigator tabBarOptions={{showLabel: false,}}>
-            <Tab.Screen options={{
-                tabBarIcon: ({color, size}) => (
-                    <MaterialCommunityIcons name='home' color={'#e46713'} size={40} />
-                )
-            }} name='Inicio' component={HomeStack} />
-            <Tab.Screen options={{
-                tabBarIcon: ({color, size}) => (
-                    <MaterialCommunityIcons name='store' color={'#e46713'} size={40} />
-                )
-            }} name='Loja' component={StoreStack} />
-            </Tab.Navigator>
+        <NavigationContainer>
+            <HomeStack />
         </NavigationContainer>
     )
 }
@@ -50,17 +39,10 @@ export default function Navigators() {
 export const HomeStack = () => {
 
     return (
-    <Stack.Navigator>
-        <Stack.Screen name='Inicio' component={Home} />
-        <Stack.Screen name='Plataforma EAD' component={Classroom} />
-        <Stack.Screen name='Contato' component={Contact} />
-    </Stack.Navigator>
-    )
-}
-
-const StoreStack  = () => {
-    return (
         <Stack.Navigator>
+            <Stack.Screen options={{ headerShown: false }} name='Inicio' component={Home} />
+            <Stack.Screen name='Plataforma EAD' component={Classroom} />
+            <Stack.Screen name='Contato' component={Contact} />
             <Stack.Screen name='Loja' component={Store} />
             <Stack.Screen name='Cursos' component={Courses} />
             <Stack.Screen name='Exames' component={Exam} />
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,88 +1,92 @@
 import React from 'react';
 import { Card, Title, Paragraph, Button } from "react-native-paper";
-import { View, Text, SafeAreaView, StyleSheet, Image, Dimensions, ScrollView } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet, Image, Dimensions, ScrollView, TouchableOpacity } from "react-native";
+import { IconButton } from 'react-native-paper';
 
 const logo = require('../../../assets/logo.png')
 const kid = require('../../../assets/kid-studying.png')
 
 function Home({ navigation }) {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.Container}>
+
+            <View>
+                <Title style={styles.Welcome}>Bem-vindo (a)!</Title>
+            </View>
+
             <ScrollView>
-                <View style={styles.Container}>
-                    <Image style={styles.Logo} resizeMode='contain' source={logo} />
-                </View>
+                <Card style={styles.Card}>
+                    <Card.Content>
+                        <Title style={styles.Title}>Plataforma EAD</Title>
+                        <Paragraph>Acesse nossa plataforma EAD diretamente do nosso aplicativo para celulares.</Paragraph>
+                    </Card.Content>
+                    <Card.Actions>
+                        <Button onPress={() => navigation.navigate('Plataforma EAD')}>
+                            Acessar
+                        </Button>
+                    </Card.Actions>
+                </Card>
 
-                <View>
-                    <Paragraph style={styles.Paragraph}>
-                        Seja muito bem-vindo (a) à aplicação móvel da Office IT.
-                        Navegue pelo menu na parte inferior da aplicação ou acesse a
-                        função desejada.
-                </Paragraph>
-                </View>
-
-                <View style={{ alignItems: 'center', justifyContent: 'center', width: '100%', height: 250, marginVertical: 25 }}>
-                    <Image style={styles.kid} resizeMode='cover' source={kid} />
-                </View>
-
-                <View style={styles.TotalView}>
-
-                    <ScrollView horizontal={true}>
-                        <Card style={styles.Card}>
-                            <Card.Content>
-                                <Title style={styles.Title}>Plataforma EAD</Title>
-                                <Paragraph>Acesse nossa plataforma EAD diretamente do nosso aplicativo para celulares.</Paragraph>
-                            </Card.Content>
-                            <Card.Actions>
-                                <Button onPress={() => navigation.navigate('Plataforma EAD')}>
-                                    Acessar
-                    </Button>
-                            </Card.Actions>
-                        </Card>
-
-                        <Card style={styles.Card}>
-                            <Card.Content>
-                                <Title style={styles.Title}>Contato</Title>
-                                <Paragraph>Fale com nossa equipe de profissionais em Brasília e em São Paulo.</Paragraph>
-                            </Card.Content>
-                            <Card.Actions>
-                                <Button onPress={() => navigation.navigate('Contato')}>
-                                    Acessar
-                    </Button>
-                            </Card.Actions>
-                        </Card>
-
-                    </ScrollView>
-                </View>
+                <Card style={styles.Card}>
+                    <Card.Content>
+                        <Title style={styles.Title}>Loja</Title>
+                        <Paragraph>Acesse nossa loja e escolha as melhores ofertas para você!</Paragraph>
+                    </Card.Content>
+                    <Card.Actions>
+                        <Button onPress={() => navigation.navigate('Loja')}>
+                            Acessar
+                        </Button>
+                    </Card.Actions>
+                </Card>
             </ScrollView>
+
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={styles.BottomContainer}>
+
+                <TouchableOpacity onPress={() => navigation.navigate('Contato')}>
+                    <Card style={styles.BottomCards}>
+                        <Card.Content>
+                            <IconButton style={styles.Icons} icon='phone-check' color={'#fff'} size={80} />
+                            <Title style={styles.BottomTitle}>Contato</Title>
+                        </Card.Content>
+                    </Card>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => navigation.navigate('Exames')}>
+                    <Card style={styles.BottomCards}>
+                        <Card.Content>
+                            <IconButton style={styles.Icons} icon='book' size={80} color={'#fff'} />
+                            <Title style={styles.BottomTitle}>Exames</Title>
+                        </Card.Content>
+                    </Card>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => navigation.navigate('Cursos')}>
+                    <Card style={styles.BottomCards}>
+                        <Card.Content>
+                            <IconButton style={styles.Icons} icon='school' size={80} color={'#fff'} />
+                            <Title style={styles.BottomTitle}>Cursos</Title>
+                        </Card.Content>
+                    </Card>
+                </TouchableOpacity>
+
+            </ScrollView>
+
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    TotalView: {
-        marginHorizontal: 25,
-    },
-
-    Logo: {
-        width: '80%',
-        height: '100%',
-        flex: 1,
-        flexWrap: 'wrap'
-    },
-
-    kid: {
-        width: 250,
-        height: 250,
-        flex: 1,
-        flexWrap: 'wrap',
-    },
 
     Container: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-        height: 100,
+        backgroundColor: '#000075',
+        flex: 1,
+    },
+
+    Welcome: {
+        color: 'white',
+        fontWeight: 'bold',
+        marginBottom: 15,
+        marginLeft: '5%'
     },
 
     Title: {
@@ -95,13 +99,38 @@ const styles = StyleSheet.create({
     },
 
     Card: {
-        width: '25%',
-        marginRight: 15,
+        width: '90%',
+        alignSelf: 'center',
+        marginBottom: 15,
+
+    },
+
+    BottomContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        marginBottom: 25,
+    },
+
+    BottomCards: {
+        width: 180,
+        height: 180,
+        marginHorizontal: 15,
+        backgroundColor: '#ffa500'
+    },
+
+    BottomTitle: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: '#fff',
+
+    },
+
+    Icons: {
+        alignSelf: 'center',
+
     }
 
 
 })
-
-const { width, height } = Dimensions.get('screen') / 4;
 
 export default Home;
